2c4da1d5795cac9115f8cac4e55b1e8d40da05e1
inturus <67243198+inturus@users.noreply.github.com>
2021-03-19 17:42:18 +0300

    modal menu updated

--- index.js
+++ index.js
@@ -9,12 +9,20 @@ sendBtn.addEventListener('click', function(event) {
 
 const menu = document.querySelector('#modal-link');
 const modal = document.querySelector('.modal');
+const menuLink = document.querySelectorAll('.menu__link');
  
 menu.addEventListener('click', function(event) {
     event.preventDefault();
 
     modal.style.display = 'block';
     document.querySelector('body').style.overflow = 'hidden';
+
+    [].forEach.call(menuLink,function(el){
+        el.addEventListener('click', function (e) {
+            modal.style.display = 'none';
+    document.querySelector('body').style.overflow = 'visible';
+        })
+    });
 })
 
 const close = document.querySelector('#close');
@@ -25,3 +33,5 @@ close.addEventListener('click', function(event) {
     modal.style.display = 'none';
     document.querySelector('body').style.overflow = 'visible';
 })
+
+
